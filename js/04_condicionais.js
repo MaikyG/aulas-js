d602145c3c76e310d7e5f1f5d8c1c3718529c95b
MaikyG <maikygabriel3@gmail.com>
2022-04-07 17:00:27 -0300

    condicionais js
    aprendendo condicionais.

---
+++ js/04_condicionais.js
@@ -0,0 +1,54 @@
+console.log("okay");
+
+/* estrutura condicional
+if(se), else(senão), else if(senão se)
+
+operadores relacionais (usandos com condicionais)
+> maior que
+< menor que
+>= maior ou igual
+<= menor ou igual
+== igualdade
+!= diferente
+*/
+
+// exemplo 1: condicional SIMPLE
+
+let numero = 10;
+
+if(numero > 0){
+    console.log(`${numero} é maior que zero`)
+}
+
+// exemplo 2: condicional composta (if/else)
+let aluno = "joão";
+let nota1 = 9.64;
+let nota2 = 9.55;
+let media = (nota1 + nota2) /2
+
+console.log(`Aluno: ${aluno}`);
+console.log(`Notas: ${nota1} e ${nota2}`);
+console.log(`Media: ${media.toFixed(2)}`);
+
+let situacao; //underfined (indefinida)
+if(media >= 7){ 
+    situacao = "aprovado";
+} else {situacao = "reprovado";
+}
+console.log(situacao);
+
+// exemplo 3 condicional ENCADEADA (if,else if, else)
+
+let desempenho;
+if(media < 3){
+    desempenho = "pessimo";
+}else if(media < 5){
+    desempenho = "ruim";
+}else if(media < 7){
+    desempenho = "regular";
+}else if(media < 9){
+    desempenho = "bom";
+}else {
+    desempenho = "otimo";
+}
+console.log(desempenho);

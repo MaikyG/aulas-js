1238bd29a063cff37e8e5b2ec8e4708e2eaa6125
MaikyG <maikygabriel3@gmail.com>
2022-04-13 14:04:01 -0300

    usando funções
    arrow function

--- js/06_funcoes.js
+++ js/06_funcoes.js
@@ -29,11 +29,57 @@ console.log(`Ano: ${ano}`);
 linha()
 
                 //parametros/argumentos
-function soma( valor1, valor2){
+// function soma( valor1, valor2){
+//     let total = valor1 + valor2;
+//     return total;
+// }
+
+// versão 2: sintax ARROw Function função seta/flecha
+const soma = (valor1, valor2) => {
     let total = valor1 + valor2;
     return total;
-}
+};
 
-console.log( soma(10,5) );
+const soma1 = (valor1, valor2) => valor1 + valor2;
+
+
+console.log( soma1(10,5) );
 // console.log( total); // erros,pois é variavel
 linha()
+
+let resultado = soma1(100, 50);
+console.log(resultado);
+    if(resultado <1000 ){
+        console.log(`abaixo da meta`)
+    };
+linha
+
+
+//1 versão
+/*     function dobra(numero){
+    let resultado = numero * 2;
+    return resultado;
+ } */
+
+
+ /* function dobra(numero){
+     return numero * 2;
+ } */
+
+//  versão 2: arrow function
+/* const dobra = (numero) => {
+    let resultado = numero * 2;
+    return resultado;
+}; */
+
+/* const dobra = (numero) => {
+    return numero * 2;
+} */
+
+
+//retorni IMPLICITO (remove {} e return)
+//obs.: como so temos um parametro (numero), podemos tirar os ()
+const dobra = numero =>  numero * 2;
+
+console.log(dobra(10));
+console.log(dobra(150));
